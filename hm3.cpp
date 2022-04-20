39968460f22390ee7ff008515789fb7d2ec7f64f
veronica <laurens.boychuk@gmail.com>
2022-04-20 17:49:16 +0200

    Add project files.

---
+++ hm3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    cout << R"(|                                                                           |)" << "\n";
+    cout << R"(|                    Boichuk Veronika                                       |)" << "\n";
+    cout << R"(|                          |                                                |)" << "\n";
+    cout << R"(|   brother Mark          / \          sister Katerina                      |)" << "\n";
+    cout << R"(|             -----      /   \       -----                                  |)" << "\n";
+    cout << R"(|                  \    /     \    /                                        |)" << "\n";
+    cout << R"(|                    \ /       \ /                                          |)" << "\n";
+    cout << R"(|                ------         ------                                      |)" << "\n";
+    cout << R"(|               /                     \                                     |)" << "\n";
+    cout << R"(|              /                       \                                    |)" << "\n";
+    cout << R"(|             /                         \                                   |)" << "\n";
+    cout << R"(|            /                           \                                  |)" << "\n";
+    cout << R"(|           /                             \                                 |)" << "\n";
+    cout << R"(| mother Diana                        father Michael                        |)" << "\n";
+    cout << R"(|          |                                     |                          |)" << "\n";
+    cout << R"(|         / \                                   / \                         |)" << "\n";
+    cout << R"(|        /   \                                 /   \                        |)" << "\n";
+    cout << R"(|       /     \                               /     \                       |)" << "\n";
+    cout << R"(|      /       \                             /       \                      |)" << "\n";
+    cout << R"(|     /         \                           /         \                     |)" << "\n";
+    cout << R"(|    /           \                         /           \                    |)" << "\n";
+    cout << R"(|grandpa Kote   grandmother Tomara   grandpa Kolya   grandmother Maria      |)" << "\n";
+}
